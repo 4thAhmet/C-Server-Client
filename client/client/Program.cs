31f50551a36d1d4875255efac11fbda8a7c76060
Ahmet <98396834+4thAhmet@users.noreply.github.com>
2024-06-04 21:57:16 +0300

    Add files via upload

---
+++ client/client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ıp, nick;
+            int port;
+            Console.Write("Ip adress giriniz: ");
+            ıp = Console.ReadLine();
+            Console.Write("Port giriniz: ");
+            port = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Nick giriniz: ");
+            nick = Console.ReadLine();
+            Socket bağlantı = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            bağlantı.Connect(IPAddress.Parse(ıp), port);
+            NetworkStream ağ = new NetworkStream(bağlantı);
+            BinaryReader okuyucu = new BinaryReader(ağ);
+            BinaryWriter yazıcı = new BinaryWriter(ağ);
+            baslangıc:
+            string mesaj;
+            Console.Write("Mesajınızı giriniz: ");
+            mesaj = Console.ReadLine();
+            yazıcı.Write(nick + " : " + mesaj);
+            Console.Write("Karşı Taraf Mesajı Bekleyiniz.");
+            string gelen = okuyucu.ReadString();
+            Console.WriteLine(nick + " : " + gelen);
+            goto baslangıc;
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip, nick;
+            int port;
+            Console.Write("Nick giriniz: ");
+            nick = Console.ReadLine();
+            Console.Write("Açılacak Port: ");
+            port = Convert.ToInt16(Console.ReadLine());
+            TcpListener dinleyeci = new TcpListener(port);
+            dinleyeci.Start();
+            Console.WriteLine("Server Başladı");
+            Console.WriteLine("Karşı Taraf Bekleniyor");
+            Socket istemci = dinleyeci.AcceptSocket();
+            Console.WriteLine("Karşı Taraf Bağlandı.");
+            NetworkStream ağ = new NetworkStream(istemci);
+            BinaryReader okuyucu = new BinaryReader(ağ);
+            BinaryWriter yazıcı = new BinaryWriter(ağ);
+            baslangıc:
+            string mesaj, gmesaj = okuyucu.ReadString();
+            Console.WriteLine("Gelen: " + gmesaj);
+            Console.Write("mesajınız: ");
+            mesaj = Console.ReadLine();
+            yazıcı.Write(nick + " : " + mesaj);
+            goto baslangıc;
+
+
+        }
+    }
+}
